3dba0631af6a2992ac97e8614b4fb95c82e9d122
bravindan <danielbravin0@gmail.com>
2020-02-07 13:37:03 +0300

    added another function with no parameters

--- functions.py
+++ functions.py
@@ -6,4 +6,16 @@ print(printme(30,40))
 def getName():
     name = input("Name?")
     print(name)
+getName()
 
+def calc():
+    len=int(input("length: "))
+    width=int(input("width: "))
+    ht=int(input("heigth: "))
+    area=len*width
+    vol=len*width*ht
+  #  return area,vol
+   # return vol
+    print(area)
+    print(vol)
+calc()
