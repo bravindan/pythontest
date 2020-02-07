a0460635d477b5d1ae87a7d2f6b0ee95875c63db
bravindan <danielbravin0@gmail.com>
2020-02-07 14:27:07 +0300

    internal documentation/commenting

--- functions.py
+++ functions.py
@@ -7,15 +7,15 @@ def getName():
     name = input("Name?")
     print(name)
 getName()
-
-def calc():
-    len=int(input("length: "))
+#working on a function with no parameters
+def calc():#function
+    len=int(input("length: "))#taking user input
     width=int(input("width: "))
     ht=int(input("heigth: "))
-    area=len*width
-    vol=len*width*ht
+    area=len*width #finding the area 
+    vol=len*width*ht #finding the volume
   #  return area,vol
    # return vol
     print(area)
     print(vol)
-calc()
+calc()#function call
